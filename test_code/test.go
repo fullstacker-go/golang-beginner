f871c52b4e78bbd0c929dfee1b46febd12753227
tanvirs <tanvirs@mkcl.org>
2021-04-21 10:32:57 +0530

    Wriiten some simple functions to add and multiply numbers with type conversion

--- test_code/test.go
+++ test_code/test.go
@@ -4,15 +4,21 @@ import "fmt"
 
 var num1 int = 5
 var num2 = 7
+var num3 float64 = 2.5
 
 func main() {
 
 	fmt.Println("Hello World")
-	add(num1, num2)
+	result := add(num1, num2)
+	fmt.Println("The result of the addition is ", result)
+	fmt.Println(multiply(num2, num3))
 }
 func add(x, y int) int {
 
 	result := x + y
 	return result
 
+}
+func multiply(a int, b float64) int {
+	return a * int(b)
 }
