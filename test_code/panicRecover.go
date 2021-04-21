30a67b4f2e05d8730540ac6b1fc6831fbe6e6b0a
tanvirs <tanvirs@mkcl.org>
2021-04-21 12:52:39 +0530

    added logic for panic situation and recover fuction

---
+++ test_code/panicRecover.go
@@ -0,0 +1,28 @@
+package main
+
+import "fmt"
+
+func main() {
+	var action int
+	fmt.Println("Enter 1 for Student and 2 for Professional")
+	fmt.Scanln(&action)
+	/*  Use of Switch Case in Golang */
+	switch action {
+	case 1:
+		fmt.Printf("I am a  Student")
+	case 2:
+		fmt.Printf("I am a  Professional")
+	default:
+		panic(fmt.Sprintf("I am a  %d", action))
+	}
+	fmt.Println("Recovered Successfully")
+	// defer func() {
+	// 	action := recover()
+	// 	fmt.Println("recovering...", action)
+	// }()
+
+	defer func() {
+		fmt.Println("recovering...")
+	}()
+
+}
