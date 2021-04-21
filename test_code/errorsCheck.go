cb3be180a115652b873af1368d81772ad4e6e772
tanvirs <tanvirs@mkcl.org>
2021-04-21 12:10:11 +0530

    checking and handling errors

--- test_code/errorsCheck.go
+++ test_code/errorsCheck.go
@@ -4,7 +4,7 @@ import "fmt"
 
 func divide(x, y float64) (float64, error) {
 	if y == 0 {
-		return 0.0, fmt.Errorf("cant divide by zero")
+		return 0.0, fmt.Errorf("can't divide by zero")
 	}
 	return x / y, nil
 }
@@ -12,7 +12,7 @@ func divide(x, y float64) (float64, error) {
 func main() {
 	quotient, error := divide(5.6, 2)
 	if error != nil {
-		fmt.Errorf("cant divide by zero")
+		fmt.Println(error)
 	}
 	fmt.Printf("%0.2f\n", quotient) // => 2.80
 }
